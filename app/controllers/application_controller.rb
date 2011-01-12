395d75e34f192d6e22ca5f7b39b8d7b4eb230b94
Imran Raja <imranraja85@gmail.com>
2011-01-12 18:55:20 -0500

    new jquey stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper :all
   helper_method :current_user
+  before_filter :authenticate_user!
 
-  private
-    def current_user
-      return @current_user if defined?(@current_user)
-      @current_user = User.first
-    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110102074336) do
+ActiveRecord::Schema.define(:version => 20110112054935) do
 
   create_table "events", :force => true do |t|
     t.string    "name"
@@ -50,11 +50,21 @@ ActiveRecord::Schema.define(:version => 20110102074336) do
   end
 
   create_table "users", :force => true do |t|
-    t.string    "name",       :limit => 80,  :default => "", :null => false
-    t.string    "company",    :limit => 80,  :default => "", :null => false
-    t.string    "email",      :limit => 128, :default => "", :null => false
-    t.string    "title",                     :default => "", :null => false
-    t.string    "department", :limit => 128
+    t.string    "name",                 :limit => 80,  :default => "", :null => false
+    t.string    "company",              :limit => 80,  :default => "", :null => false
+    t.string    "title",                               :default => "", :null => false
+    t.string    "department",           :limit => 128
+    t.string    "email",                               :default => "", :null => false
+    t.string    "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string    "password_salt",                       :default => "", :null => false
+    t.string    "reset_password_token"
+    t.string    "remember_token"
+    t.datetime  "remember_created_at"
+    t.integer   "sign_in_count",                       :default => 0
+    t.datetime  "current_sign_in_at"
+    t.datetime  "last_sign_in_at"
+    t.string    "current_sign_in_ip"
+    t.string    "last_sign_in_ip"
     t.timestamp "updated_at"
     t.timestamp "created_at"
   end
