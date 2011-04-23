57534b05538bc47de49f17d6177d9d3d5180552c
Imran Raja <imranraja85@gmail.com>
2011-04-23 13:33:39 -0400

    removed delted files

--- app/views/tasks/move_to_in_progress.js
+++
@@ -1,8 +0,0 @@
-var task_started = $("#<%= @task.id %>"); //find the id of the task selected
-task_started.text("Complete Task");       //Update the link text
-var html = task_started.parent().parent().html(); //find the container for the task and extract html
-
-var in_progress_div = $('.tasks-in-progress-list');
-task_started.parent().parent().remove();  //remove that task
-
-in_progress_div.prepend("<li>" + html + "</li>"); //add the task to the 'tasks in progress' list
