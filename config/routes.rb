8ed648a1b6d99c7d0fbd65d58731ee29e9ed22a2
Imran Raja <imranraja85@gmail.com>
2011-11-19 21:34:32 -0500

    quick fix to the login..

--- config/routes.rb
+++ config/routes.rb
@@ -78,8 +78,8 @@ Manageme::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+  root :to => "dashboard#index"
   #root :to => "home#index"
-  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
